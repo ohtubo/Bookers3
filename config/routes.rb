67e78d3ee0ecbfa414afa8613f1ead1528941c8c
ohtubo <brte14020@gmail.com>
2021-06-28 08:45:47 +0000

    [Add]_TopAdout_Bookers3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :users, onry[:index, :show, :edit, :update, :delete]
+  root to: 'home#top'
+  get 'home/about'
   devise_for :users
+  resources :users, only:[:index, :show, :edit, :update, :delete]
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
