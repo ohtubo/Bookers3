d65b36dc27e39f02e7d066928e4693043fc055d0
ohtubo <brte14020@gmail.com>
2021-06-29 14:51:30 +0000

    [Add]_Userlist_Bookers3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
