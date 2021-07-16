e3255404f82d67e061010e77608d84e7e1feca1e
ohtubo <brte14020@gmail.com>
2021-07-16 14:59:30 +0000

    [Add]_comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,29 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#ブートストラップ用のキー
+Refile.secret_key = '57b24cf60dc992e4fc23164060e39e1df8901d5bed6cfaee3d9de6dea1d61951403639f1c4f807f51e54222c4d3aed091b28ed02dc2da25c9fe08dcf05c16d46'
